75a4c77b7543ed6b91caac0803f433f00c68c70d
Colter Nakai McAddis <cnm62@nau.edu>
2020-10-06 18:29:41 -0700

    Create basic packaging facilities and structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.adoc", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bbetter",
+    version="0.0.1",
+    author="C. Nakai McAddis",
+    author_email="nakai.mcaddis@gmail.com",
+    description="Command line interaction with BBLearn",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cnakai/bbetter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
